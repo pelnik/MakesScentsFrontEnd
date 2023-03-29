00d5bb1a357cf6484deaeebcba8762e2988d69dc
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-03-29 14:22:42 -0500

    adding some initial setup

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,26 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import { Navbar, Home, Routines, MyRoutines, Activities, LoginRegister, EditRoutine, AttachRoutineForm, EditRoutineActivity } from "./";
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Navbar, Home } from './';
 
+import { getTokenFromLocalStorage } from '../utils';
 
 const Main = () => {
+  const [token, setToken] = useState('');
 
+  useEffect(() => {
+    setToken(getTokenFromLocalStorage());
+  }, []);
 
-    return(
-        <div id="main">
-            <Navbar />
-            <div id="page">
-                <Routes>
-                    <Route exact path="/" element={<Home/>}/>
-                </Routes>
-            </div>
-        </div>
-    )
-    }
+  return (
+    <div id="main">
+      <Navbar />
+      <div id="page">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
 
-    export default Main
+export default Main;
