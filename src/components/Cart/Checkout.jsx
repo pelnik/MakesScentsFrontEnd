90676962cd2e9f5fe055f1da1c52f7e356d17253
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-04-05 10:15:49 -0500

    checkout and confirmation pages done

--- src/components/Cart/Checkout.jsx
+++ src/components/Cart/Checkout.jsx
@@ -48,13 +48,13 @@ function Checkout({ token, cart, setCart, getCart }) {
           <h1>Checkout</h1>
           <p>Thank you for shopping with us!</p>
           {!hasItems ? (
-            <div className="checkout-total" id="checkout-no-items">
+            <>
               <p>It looks like you don't have any items!</p>
               <p>
                 Click any buttons in the Navbar to go back, or{' '}
                 <Link to="/">click here</Link> to go back.
               </p>
-            </div>
+            </>
           ) : (
             <div className="checkout-total" id="checkout-with-items">
               <p>Your total is: {dollarTotal}</p>
