0d2dcb2dea9811b2d0ec25e66e358ccb243d190c
Jieun Kim <jieunkim1379@gmail.com>
2023-04-10 14:41:16 -0400

    changed N to one size'

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -290,7 +290,13 @@ function Products({
       ? searchedDisplay
       : products;
 
-  console.log(showCart, '###');
+  const oneSize = (size) => {
+    if (size === 'N') {
+      return 'One Size';
+    } else {
+      return size;
+    }
+  };
 
   useEffect(() => {
     getAllProductsPage();
@@ -301,21 +307,21 @@ function Products({
   }, []);
 
   return (
-    <div id="products-page-container">
-      <div id="products-header">
-        <div id="products-header-left">
+    <div id='products-page-container'>
+      <div id='products-header'>
+        <div id='products-header-left'>
           <h1>Products</h1>
           <input
-            id="products-search"
-            type="text"
-            placeholder="Search for Product"
+            id='products-search'
+            type='text'
+            placeholder='Search for Product'
             value={searchTerm}
             onChange={searchHandle}
           />
         </div>
         {user.is_admin ? (
           <button
-            className="add-button product-button"
+            className='add-button product-button'
             onClick={() => {
               navigate('/products/new');
             }}
@@ -324,61 +330,61 @@ function Products({
           </button>
         ) : null}
       </div>
-      <div id="side-by-side">
-        <div id="products-filter">
+      <div id='side-by-side'>
+        <div id='products-filter'>
           <h2>Filters</h2>
           <CategoryFilter token={token} user={user} />
           <br />
-          <ul className="category-list">
+          <ul className='category-list'>
             {categories.map((category, idx) => {
               return (
                 <li key={`category${idx}`}>
                   <input
-                    type="checkbox"
+                    type='checkbox'
                     id={category.id}
                     name={category.category_name}
                     value={category.category_name}
                     onChange={filterHandler}
                   />
-                  <label htmlFor="category">{category.category_name}</label>
+                  <label htmlFor='category'>{category.category_name}</label>
                 </li>
               );
             })}
           </ul>
         </div>
-        <div id="products-list">
+        <div id='products-list'>
           {productsList.length ? (
             productsList.map((product, idx) => {
               return (
-                <div id="products-container" key={`products${idx}`}>
+                <div id='products-container' key={`products${idx}`}>
                   <div
-                    className="product-detail"
+                    className='product-detail'
                     onClick={() => {
                       setSelectedProduct({ product_id: product.id });
                       navigate(`/products/${product.id}`);
                     }}
                   >
-                    <div className="product-image-box">
+                    <div className='product-image-box'>
                       {product.inventory === 0 ? (
-                        <span id="sold-out-icon">SOLD OUT</span>
+                        <span id='sold-out-icon'>SOLD OUT</span>
                       ) : null}
                       <img
                         src={product.pic_url}
-                        id="product-pic"
-                        alt="pic of candle product"
+                        id='product-pic'
+                        alt='pic of candle product'
                       />
                     </div>
-                    <div className="product-text-detail">
+                    <div className='product-text-detail'>
                       <h4>{product.name}</h4>
-                      <h5>Size: {product.size}</h5>
-                      <h3 className="important-product-detail">
+                      <h5>Size: {oneSize(product.size)}</h5>
+                      <h3 className='important-product-detail'>
                         {product.price}
                       </h3>
                     </div>
                   </div>
                   {product.inventory !== 0 && token ? (
-                    <div className="add-cart-container loader-container">
-                      <div className="add-shopping-cart-icon-container">
+                    <div className='add-cart-container loader-container'>
+                      <div className='add-shopping-cart-icon-container'>
                         <AddShoppingCartIcon
                           className={
                             showCart[product.id].loading
@@ -422,12 +428,12 @@ function Products({
                                 ? null
                                 : handleCartInputChange(evt, product.id);
                             }}
-                            type="number"
+                            type='number'
                             max={product.inventory}
                             value={showCart[product.id].amountToAdd}
                           />
                           <button
-                            type="submit"
+                            type='submit'
                             className={
                               showCart[product.id].loading
                                 ? 'fade-loader cart-button'
@@ -444,22 +450,22 @@ function Products({
                         </div>
                       ) : null}
                       {showCart[product.id].error ? (
-                        <p className="cart-warning">
+                        <p className='cart-warning'>
                           {showCart[product.id].error}
                         </p>
                       ) : null}
                       {showCart[product.id].loading ? (
-                        <div className="spinner-container">
+                        <div className='spinner-container'>
                           <Oval
-                            className="loading-spinner"
+                            className='loading-spinner'
                             height={'2em'}
                             width={'2em'}
-                            color="#db7c5a"
+                            color='#db7c5a'
                             wrapperStyle={{}}
-                            wrapperClass=""
+                            wrapperClass=''
                             visible={true}
-                            ariaLabel="oval-loading"
-                            secondaryColor="#db7c5a"
+                            ariaLabel='oval-loading'
+                            secondaryColor='#db7c5a'
                             strokeWidth={20}
                             strokeWidthSecondary={20}
                           />
@@ -468,13 +474,13 @@ function Products({
                     </div>
                   ) : null}
                   {user.is_admin ? (
-                    <div className="admin-product-card">
-                      <div id="product-inventory">
+                    <div className='admin-product-card'>
+                      <div id='product-inventory'>
                         <p>Inventory: {product.inventory}</p>
                       </div>
-                      <div className="product-buttons-container">
+                      <div className='product-buttons-container'>
                         <button
-                          className="product-button"
+                          className='product-button'
                           onClick={() => {
                             setSelectedProduct({
                               product_id: product.id,
@@ -490,7 +496,7 @@ function Products({
                           Edit
                         </button>
                         <button
-                          className="product-button"
+                          className='product-button'
                           onClick={() => {
                             removeProduct(product.id);
                           }}
